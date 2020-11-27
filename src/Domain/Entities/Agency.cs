bee5263a7e05d2de2447088bfe9a0674cced9621
Aljur-Pavel-Dubina <pavel.dubina@aljur.com>
2020-11-27 14:26:26 +0300

    Create Domain Entities

---
+++ src/Domain/Entities/Agency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class Agency
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string ContactInformation { get; set; }
+        public string Status { get; set; }
+        public string LegalEntityInformation { get; set; }
+        public IList<Agent> Agents { get; set; }
+    }
+}
