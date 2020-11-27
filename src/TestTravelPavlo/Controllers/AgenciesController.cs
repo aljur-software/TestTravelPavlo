e0b3002aa2c96867171562e5ac1bdd815228c6c7
Aljur-Pavel-Dubina <pavel.dubina@aljur.com>
2020-11-27 13:14:08 +0300

    Create empty Web API project

---
+++ src/TestTravelPavlo/Controllers/AgenciesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestTravelPavlo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AgenciesController : ControllerBase
+    {
+
+        private readonly ILogger<AgenciesController> _logger;
+
+        public AgenciesController(ILogger<AgenciesController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
