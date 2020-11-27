b1b6a2a6c9bb812576d43b34370d38974d60877e
Aljur-Pavel-Dubina <pavel.dubina@aljur.com>
2020-11-27 16:50:45 +0300

    Create TravelRepository

---
+++ src/Infractructure/Repositories/TravelRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infractructure.Repositories
+{
+    public class TravelRepository<T> : IRepository<T>where T: class
+    {
+        private readonly DbSet<T> _dbSet;
+        private readonly DbContext _context;
+      
+        public TravelRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+       
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+        public virtual IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public virtual void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public virtual Task SaveChangesAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
