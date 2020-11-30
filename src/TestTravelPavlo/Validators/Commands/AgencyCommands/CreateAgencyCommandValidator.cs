eab8ab801ddfb2d58662c754184a2925a4782cfe
Aljur-Pavel-Dubina <pavel.dubina@aljur.com>
2020-11-30 16:34:39 +0300

    Create AgenciesController. Create Command validation

---
+++ src/TestTravelPavlo/Validators/Commands/AgencyCommands/CreateAgencyCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Commands.AgencyCommands;
+using FluentValidation;
+
+namespace TestTravelPavlo.Validators.Commands.AgencyCommands
+{
+    public class CreateAgencyCommandValidator : AbstractValidator<CreateAgencyCommand>
+    {
+        public CreateAgencyCommandValidator()
+        {
+            RuleFor(_ => _.Name).NotNull().NotEmpty();
+        }
+    }
+}
